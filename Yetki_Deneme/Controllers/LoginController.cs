ebec3608275821bde4a32f6fab15a24d36444952
Hasancan <fatihmehmetsayin@gmail.com>
2020-04-11 14:05:50 +0300

    Add project files.

---
+++ Yetki_Deneme/Controllers/LoginController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Yetki_Deneme.Model;
+
+namespace Yetki_Deneme.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IActionResult Login(string username, string pass)
+        {
+            UserModel login = new UserModel();
+            login.UserName = username;
+            login.Password = pass;
+            IActionResult response = Unauthorized();
+            var user = AuthenticateUser(login);
+
+            if (user != null)
+            {
+                var tokenStr = GenerateJSONWebToken(user);
+                response = Ok(new { token = tokenStr });
+            }
+            return response;
+         }
+
+        private string GenerateJSONWebToken(UserModel userinfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+
+                new Claim(JwtRegisteredClaimNames.Sub,userinfo.UserName),
+                new Claim(JwtRegisteredClaimNames.Email,userinfo.EmailAdres),
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+
+            };
+
+            var token = new JwtSecurityToken(
+
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["jwt:Issuer"],
+                claims,
+                expires: DateTime.Now.AddHours(2),
+                signingCredentials: credentials);
+
+            var encodetoken = new JwtSecurityTokenHandler().WriteToken(token);
+            return encodetoken;
+        }
+
+        [Authorize]
+        [HttpPost("Post")]
+        public string Post()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            IList<Claim> claim = identity.Claims.ToList();
+            var userName = claim[0].Value;
+            return "Welcome To:" + userName;
+
+
+        }
+
+
+        [Authorize]
+        [HttpGet("GetValue")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+
+            return new string[] { "Value1", "Value2" };
+        }
+
+        private UserModel AuthenticateUser(UserModel login)
+        {
+            UserModel user = null;
+            if (login.UserName == "fatihmehmetsayin" && login.Password == "123")
+            {
+                user = new UserModel { UserName = "fatihmehmetsayin", EmailAdres = "fatihmehmetsayin@gmail.com", Password = "123" };
+            }
+            return user;
+        }
+    }
+}
